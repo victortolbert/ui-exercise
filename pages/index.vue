fc969df1403358fb68a4b22a5380b0d15a1c8f99
Victor Tolbert <victor.tolbert@gmail.com>
2018-10-02 20:37:35 -0400

    Render the messages from the mock api to the page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="container">
-    <a href="/api/messages">Messages API</a>
-  </div>
+  <table>
+    <tr v-for="message in messages" :key="message.id">
+      <td><input type="checkbox" /></td>
+      <td>{{message.sender}}</td>
+      <td>
+        <span v-for="tag in message.tags" :key="tag">
+          {{tag}}
+        </span>
+      </td>
+      <td>{{message.subject}}</td>
+      <td>{{message.date}}</td>
+    </tr>
+  </table>
 </template>
+
+<script>
+  export default {
+    async asyncData({ app }) {
+      let messages = await app.$axios.$get(`/api/messages`)
+      return { messages: messages }
+    }
+  }
+</script>
+
+
